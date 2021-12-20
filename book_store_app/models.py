0a9c7b4bb2d18d107bc2ccf1353d57926b27faaf
anasnadeem <anas5678go@gmail.com>
2021-12-20 22:25:28 +0530

    Project base and book_store_app with models added.

---
+++ book_store_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=100)
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=250)
+    cash_price = models.IntegerField()
+    coin_price = models.IntegerField()
+    # image = models.ImageField()
+
+class BookDescription(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    # more_image = 
+    # video_or_gif = 
+    publisher = models.CharField(max_length=250, blank=True)
+    published_date = models.DateField(blank=True)
+    mass = models.IntegerField(blank=True)
+    author_name = models.CharField(max_length=200, blank=True)
